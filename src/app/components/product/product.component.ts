7f2cbfbfda2021cced0c1dc87ab09b5354ef2bf4
Sedat Ozturk <sedatozturk63@yandex.com>
2021-05-24 11:10:48 +0300

    Commit1

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -14,7 +16,8 @@ export class ProductComponent implements OnInit {
   dataLoaded = false;
   filterText="";  
 
-  constructor(private productService:ProductService, private activatedRoute:ActivatedRoute) { } 
+  constructor(private productService:ProductService, private activatedRoute:ActivatedRoute,
+    private toastrService:ToastrService, private cartService:CartService) { } 
 
   ngOnInit(): void {
 
@@ -43,4 +46,14 @@ export class ProductComponent implements OnInit {
     })
     
   }
+
+  addToCard(product:Product){
+    if(product.productId===1){
+      this.toastrService.error("Hata","Sepete eklenemez")
+    }else{
+      this.toastrService.success("Sepete eklendi",product.productName)
+      this.cartService.addToCart(product);
+    }    
+  }
+  
 }
